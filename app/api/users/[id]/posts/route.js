c9676ddba70282386c59f1cb8259b4a674d601ca
Balaji <balajik.hgi@gmail.com>
2023-05-19 18:08:41 -0400

    update db query syntax

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -4,9 +4,12 @@ import Prompt from '@models/prompt';
 export const GET = async (request, { params }) => {
   try {
     await connectToDB();
+    // const prompts = await Prompt.find({
+    //   creator: params.id,
+    // }).populate('creator');
     const prompts = await Prompt.find({
       creator: params.id,
-    }).populate('creator');
+    });
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
